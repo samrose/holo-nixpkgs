0fb0a6ba3350b1cc9cfb66bacd21671d1e30f157
PJ <pj@imagine-nyc.com>
2020-06-04 15:14:55 +0200

    profiles/hydra/master: limit max_concurrent_evals to 3 for 16GB droplets

--- profiles/logical/holo/hydra/master/default.nix
+++ profiles/logical/holo/hydra/master/default.nix
@@ -67,7 +67,7 @@ in
       binary_cache_public_uri = https://cache.holo.host
       evaluator_max_heap_size = ${toString (4 * 1024 * 1024 * 1024)}
       log_prefix = https://cache.holo.host/
-      max_concurrent_evals = 12
+      max_concurrent_evals = 3
       max_output_size = 17179869184
       server_store_uri = https://cache.holo.host?local-nar-cache=/var/cache/hydra/nar-cache
       store_uri = s3://${wasabiBucket}?endpoint=${wasabiEndpoint}&log-compression=br&ls-compression=br&parallel-compression=1&secret-key=/var/lib/hydra/queue-runner/keys/${signingKeyName}/secret&write-nar-listing=1
