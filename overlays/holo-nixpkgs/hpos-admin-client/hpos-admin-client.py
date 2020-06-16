03e82706e6b966c5105461803cabae96b2641525
Alastair Ong <alastair.ong@gmail.com>
2020-06-16 13:58:39 +0100

    overlays/hpos-admin-client.py: Add factory reset command

--- overlays/holo-nixpkgs/hpos-admin-client/hpos-admin-client.py
+++ overlays/holo-nixpkgs/hpos-admin-client/hpos-admin-client.py
@@ -52,5 +52,13 @@ def get_status(ctx):
     print(request(params, 'GET', '/status').json())
 
 
+@cli.command(help='Initiate a factory reset')
+@click.pass_context
+def factory_reset(ctx):
+    res = request(ctx, 'POST', '/reset')
+    if res.status_code == 400:
+        print("Failed to reset device")
+
+
 if __name__ == '__main__':
     cli(obj={})
